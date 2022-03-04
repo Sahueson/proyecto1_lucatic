58f32039dc17d9362aaddf198b7ad258136ce7e0
Juanru <Juanru@DESKTOP-SDDMM3P>
2022-03-04 02:19:09 +0100

    Prueba 1

--- proyecto1_lucatic/src/proyecto1_lucatic/control/Main.java
+++ proyecto1_lucatic/src/proyecto1_lucatic/control/Main.java
@@ -3,7 +3,8 @@ package proyecto1_lucatic.control;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Probando");
 
 	}
 
